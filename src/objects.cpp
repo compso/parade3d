ef50e3469f2dfd57aef842da5bae43743ea2a27f
Ashley <ashley.retallack@gmail.com>
2009-05-04 00:56:12 +0100

    Added License to source files

--- src/objects.cpp
+++ src/objects.cpp
@@ -1,3 +1,19 @@
+/************************************************
+
+    Parade3d -- 3d model viewer
+    Copyright (C) 2009 Ashley James Retallack
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+*************************************************/
 
 #include <qgl.h>
 #include "objects.h"
